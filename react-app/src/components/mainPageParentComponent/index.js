9f96e95e7a47c6280047e9d65a9a446f7dabccb1
030820acc <030820acc@gmail.com>
2022-01-11 18:43:16 -0500

    main page rendered

---
+++ react-app/src/components/mainPageParentComponent/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ShowAllMessages from "../ShowAllMessages";
+import ShowChannel from "../ShowChannel";
+
+
+function Main() {
+  //NEED TO ADD SHOW USERS ONCE WRITTEN
+    
+    return (
+      <div>
+          <ShowChannel />
+          <ShowAllMessages />
+      </div>
+    );
+  }
+  
+  export default Main;
+  
