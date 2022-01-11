740544d88a1186e563684e3399e0cce55af27aa5
sezder <84882854+sezder@users.noreply.github.com>
2022-01-11 13:40:06 -0700

    show channel list component

--- react-app/src/components/ShowChannel/index.js
+++ react-app/src/components/ShowChannel/index.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useParams } from "react-router-dom";
+import { getAllChannels } from "../../store/channels";
+import "./ShowChannel.css";
 
 const ShowChannel = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+  let { serverId, categoryId, channelId } = useParams();
+  serverId = Number(serverId);
+  categoryId = Number(categoryId);
+  channelId = Number(channelId);
+
+  const channelsObj = useSelector((state) => state.channels);
+  let channelsArr;
+  if (channelsObj) {
+    channelsArr = Object.values(channelsObj);
+  }
+
+  useEffect(() => {
+    dispatch(getAllChannels());
+  }, [dispatch, serverId, categoryId, channelId]);
+
+  return (
+    <ul>
+      {channelsArr.map((channel) => (
+        <NavLink
+          to={`/servers/${serverId}/categories/${categoryId}/channels/${channelId}/messages`}
+        >
+          <li key={channel?.id}>{channel?.name}</li>
+        </NavLink>
+      ))}
+    </ul>
+  );
 };
 
 export default ShowChannel;
