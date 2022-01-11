c99c3df3a616ac9688c7a182245d98689749fe40
tforde4623 <sltebr33zy@gmail.com>
2022-01-11 11:28:40 -0600

    fixed logout bug

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { logout } from '../../store/session';
 
 const LogoutButton = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.session.user);
+
+
   const onLogout = async (e) => {
     await dispatch(logout());
     // return <Redirect to="/"></Redirect>
   };
 
-  return <button onClick={onLogout}>Logout</button>;
+  return user && <button onClick={onLogout}>Logout</button>;
 };
 
 export default LogoutButton;
