8cb9f4911e3cdfaed7225abb14abb749171e601e
tforde4623 <sltebr33zy@gmail.com>
2022-01-13 15:42:02 -0600

    added default channel to server

--- app/seeds/servers.py
+++ app/seeds/servers.py
@@ -2,7 +2,10 @@ from app.models import db
 from app.models.servers import Server
 
 def seed_servers():
-  server1 = Server(name="Dog Appreciation", owner_id=1, icon_url="https://source.unsplash.com/Sg3XwuEpybU")
+  server1 = Server(name="Dog Appreciation", 
+                   owner_id=1, 
+                   icon_url="https://source.unsplash.com/Sg3XwuEpybU", 
+                   default_channel=None)
   db.session.add(server1)
   db.session.commit()
 
