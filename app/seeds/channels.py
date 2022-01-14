29fad855b52efefa0464c81d05475cf3afbe194f
Asia <asia.a.le.work@gmail.com>
2022-01-14 09:49:58 -0800

    files seeded

--- app/seeds/channels.py
+++ app/seeds/channels.py
@@ -1,6 +1,21 @@
 from app.models import db
 from app.models.channels import Channel
 
+DEFAULT_CHANNELS = [
+  Channel(name='Dog Photos', server_id=1, category_id=1),
+  Channel(name='Tiktok Links', server_id=1, category_id=1),
+  Channel(name='THE CAT KINGDOM', server_id=2, category_id=2),
+  Channel(name='Valorant', server_id=3, category_id=3),
+  Channel(name='Rainbow Six Siege', server_id=3, category_id=3),
+  Channel(name='Minecraft', server_id=3, category_id=4),
+  Channel(name='Slime Rancher', server_id=3, category_id=4),
+  Channel(name='Breakfast', server_id=4, category_id=5),
+  Channel(name='Dessert', server_id=4, category_id=5),
+  Channel(name='Childhood Photos', server_id=4, category_id=6),
+  Channel(name='Tool Setup', server_id=4, category_id=7),
+  Channel(name='Vi x Caitlyn', server_id=9, category_id=11)
+]
+
 def seed_channels():
   dogPhotos = Channel(name="Dog Photos", server_id=1, category_id=1)
   db.session.add(dogPhotos)
