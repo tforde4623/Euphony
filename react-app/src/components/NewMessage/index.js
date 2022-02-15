3667baab5a08642dd5250133ce576990a25bce51
tforde4623 <sltebr33zy@gmail.com>
2022-02-14 20:47:54 -0600

    added error handling and simple data validation for messages

--- react-app/src/components/NewMessage/index.js
+++ react-app/src/components/NewMessage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Redirect, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./NewMessage.css";
@@ -7,9 +7,33 @@ const NewMessage = ({ sock }) => {
   const { channelId } = useParams();
   const [content, setContent] = useState("");
   // const [errors, setErrors] = useState([]);
+  const [contentErr, setContentErr] = useState('');
 
   const user = useSelector((state) => state.session.user);
   const userId = user?.id;
+
+  // TODO: MEMORY???
+  useEffect(() => {
+    // listen for emissions on errors
+    let timeout = null
+    if (sock) {
+      sock.on('err', (data) => {
+        if (data.content) {
+          setContentErr(data.content);
+
+          // we want to get rid of it after 3
+          timeout = setTimeout(() => {
+            setContentErr();
+          }, 3000);
+        }
+      });
+    }
+
+    return () => {
+      clearTimeout(timeout)
+    };
+  }, [sock]);
+
   if (!userId) return <Redirect to="/"></Redirect>;
 
   const resetContent = () => {
@@ -45,7 +69,8 @@ const NewMessage = ({ sock }) => {
 
         {/* Message Input */}
         <textarea
-          placeholder="Send a message..."
+          className={contentErr ? 'input-err' : null}
+          placeholder={contentErr ? 'Some message is required to send!' : "Send a message..."}
           type="text"
           name="content"
           value={content}
